1f3dd7d78e9d6feeb1753fd8800816b443c84fef
Erick <erickprepa1xd@gmail.com>
2025-12-09 20:37:42 -0600

    App de musica completo Erick

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  spotify: {
+    // Reemplaza los valores siguientes con tus credenciales de Spotify
+    CLIENT_ID: 'YOUR_SPOTIFY_CLIENT_ID',
+    CLIENT_SECRET: 'YOUR_SPOTIFY_CLIENT_SECRET',
+    AUTH_API_URL: 'https://accounts.spotify.com/api/token',
+    API_URL: 'https://api.spotify.com/v1'
+  }
+};
